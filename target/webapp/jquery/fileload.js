0792f9383286b528bc2d36d9b2e966c9232d33bb
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2014-09-13 20:23:16 +0200

    Edit default level

--- target/webapp/jquery/fileload.js
+++
@@ -1,8 +0,0 @@
-
-
-function copyLink() {
-    var link = document.getElementById("linking").innerHTML;
-    var topDoc = window.top.document;
-    var urlElem = topDoc.getElementById('urlLink');
-    if (urlElem) urlElem.value = link;
-}
--- target/webapp/jquery/mytables.js
+++
@@ -1,6 +0,0 @@
-
-function colorTables(){
-	$('tbody').children('tr').each(function(index){
-		if(index % 2 == 1) this.className = "even_row";
-	});
-}
