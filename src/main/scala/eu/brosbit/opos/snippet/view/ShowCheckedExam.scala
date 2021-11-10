1eda0c342e58966b3c35b233b2ca0a1d29511f92
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2021-11-10 14:27:53 +0100

    fix show answers exams for student

--- src/main/scala/eu/brosbit/opos/snippet/view/ShowCheckedExam.scala
+++ src/main/scala/eu/brosbit/opos/snippet/view/ShowCheckedExam.scala
@@ -3,7 +3,8 @@ package eu.brosbit.opos.snippet.view
 import eu.brosbit.opos.lib.{Formater, ZeroObjectId}
 import _root_.net.liftweb.util._
 import Helpers._
-import eu.brosbit.opos.model.edu.{AnswerWorkItem, Exam, ExamAnswer, QuizQuestion, Work, WorkAnswer}
+import eu.brosbit.opos.model.edu.{AnswerItem, AnswerWorkItem, Exam, ExamAnswer, QuizQuestion, Work, WorkAnswer}
+import net.liftweb.common.Empty
 import net.liftweb.http.{S, SHtml}
 import net.liftweb.json.JsonDSL._
 import net.liftweb.util.CssSel
@@ -27,30 +28,38 @@ class ShowCheckedExam extends BaseSnippet {
       //val quiz = Quiz.find(idQuiz).getOrElse(Quiz.create)
       // println("=========  quiz: " + quiz.title + " ; length:  " + quiz.questions.length.toString)
       if(quizzes.length < 1) S.redirectTo("/view/exams?Error")
-      val questMap = quizzes.map(qi => ( qi.q.toString -> qi.p)).toMap
-
+      val questMap = quizzes.map(qi =>  qi.q.toString -> qi.p).toMap
+      val answerMapA = exAns.answers.map(a => a.q -> a.a).toMap
       val questions = QuizQuestion.findAll("_id" -> ("$in" -> questMap.keySet.toList))
-      val questionsItems = questions.map(qu =>  (qu, questMap(qu._id.toString)))
+      val questionsItems = questions.map(qu =>  {
+        val id = "_"+qu._id.toString
+        val answerIt = exAns.answers.find(_.q == id).getOrElse(AnswerItem("", "", 0))
+        (qu, questMap(qu._id.toString), answerIt.a, answerIt.p)
+      })
         "#descript *" #> exam.description &
         "#subject *" #> exam.subjectName &
         "#endTime *" #> Formater.formatDate(new Date(exam.end)) &
         "#info *" #> exAns.info &
-        "#test" #> questionsItems.map( q => "div" #> mkQuestHTML(q._1, q._2))
+        "#test" #> questionsItems.map( q => "div" #> mkQuestHTML(q._1, q._2, q._3, q._4))
   }
 
-  protected def mkQuestHTML(question:QuizQuestion, pkt:Int) = {
+  protected def mkQuestHTML(question:QuizQuestion, pkt:Int, ans:String, getPkt:Int) = {
 
     nr += 1
     <section class="question" id={"_" + question._id.toString } name={"zad_" + nr.toString}>
       <div class="panel panel-info">
         <div class="panel-heading">
           <span class="quizNr">{nr.toString}</span>
-          Zadanie  <span class="badge" title="Punkty">{pkt.toString} pkt.</span></div>
+          Zadanie  <span class="badge" title="Punkty">{pkt.toString} pkt.</span>
+          <span class="badge leftPkt" title="Uzyskane">Uzyskane: {getPkt} pkt</span>
+        </div>
         <div class="panel-body">
           <div class="questionText">
             {Unparsed(question.question)}
           </div>
           <div>{createAnswers(question.answers, question.fake)}</div>
+          <div class="yourAns"><h4>Twoja odpowiedź:</h4>
+            <p>{Unparsed(ans.split(";#;;#;").mkString("<br/>"))}</p></div>
         </div>
       </div>
     </section>
@@ -59,16 +68,10 @@ class ShowCheckedExam extends BaseSnippet {
   protected def createAnswers(good:List[String], fake:List[String]) = {
 //todo: show test answer possible and answer added by user, show points acquired
     val name = "quest_" + nr.toString
-    if(fake.isEmpty) {
-      if(good.nonEmpty) <p class="info"></p>
-      else  <p class="info"></p>
-    }
+    if(fake.isEmpty)  <span></span>
     else {
-
-      val all = (fake ++ good).sortWith(_ < _) .map(s => <ul>
-        <li>{Unparsed(s)} </li> </ul>)
-
-      <div class="answerBox">{all}</div>
+      val all = (fake.map(s => <li style="color:red;">{Unparsed(s)}</li>) ++ good.map(s => <li style="color:green;">{Unparsed(s)}</li>))
+      <div class="answerBox"><ul>{all}</ul></div>
     }
 
   }
